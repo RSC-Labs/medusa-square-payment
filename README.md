b3e18693825dfdf67f9312fa3193a46b067e173a
Radoslaw Szymkiewicz <szymkiewicz.radoslaw@gmail.com>
2025-04-18 15:49:55 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,128 @@
-# medusa-square-payment
-Payment provider for Square
+# Medusa Square Payment
+
+## What is it?
+
+Medusa Square Payment is a basic integration of payment provider for Square.
+
+## Installation
+
+1. Install plugin by adding to your `package.json`:
+
+**Warning**
+
+```json
+...
+"@rsc-labs/medusa-square-payment": "0.0.1" // or other available version
+...
+```
+and execute install, e.g. `yarn install`.
+
+2. Add plugin to your `medusa-config.js` (**Note** - please notice that you need to add it to payment plugin):
+
+```js
+...
+  plugins: [
+    {
+      resolve: "@rsc-labs/medusa-square-payment",
+      options: {
+        token: <app-token>,
+        environment: <env-definition>,
+      },
+    }
+  ],
+  modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@rsc-labs/medusa-square-payment/providers/square-payment",
+            id: "square-payment",
+            options: {
+              token: <app-token>,
+              environment: <env-definition>,
+            },
+          }
+        ]
+      },
+    },
+...
+```
+
+## Overview
+
+The basic implementation of Square payment provider gives the possibility to make a payment in your storefront.
+
+## Configuration
+
+Plugin uses 1 required parameter and 1 optional:
+
+- `token` - required parameter which you can find in your Square Developer Dashboard
+- `environment` - optional parameter - it can be set to `sandbox`, `production` or not set at all. You can use it to test with your `sandbox` environment.
+
+
+After above configuration, you can then add the payment provider to your reqion.
+
+## Storefront
+
+We recommend using `react-square-web-payments-sdk` package on your storefront as it simplifies the implementation a lot.
+Here is the example of using credit card as payment:
+
+```tsx
+import { PaymentForm, CreditCard as SquareCreditCard } from 'react-square-web-payments-sdk';
+...
+
+const handleSquareSubmit = async (token: string) => {
+    setIsLoading(true)
+    try {
+      if (!activeSession) {
+        await initiatePaymentSession(cart, 
+          {
+            provider_id: selectedPaymentMethod,
+            data: {
+              cartId: cart?.id,
+              token: token
+            }
+          })
+      }
+
+      return router.push(
+        pathname + "?" + createQueryString("step", "review"),
+        {
+          scroll: false,
+        }
+      )
+    } catch (err: any) {
+      setError(err.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+{isSquare(selectedPaymentMethod) &&
+  <div>
+    <PaymentForm
+      applicationId="<application-id>"
+      cardTokenizeResponseReceived={(token, verifiedBuyer) => {
+        handleSquareSubmit(token.token!);
+      }}
+      locationId='XXXXXXXXXX'
+    >
+      <SquareCreditCard/>
+    </PaymentForm>
+  </div>
+}
+```
+
+`<application-id>` - you can retrieve it from your Square Developer Dashboard.
+
+## Limitations
+
+Plugin does not support refunds and cancels. It has been tested using only credit card - when authorized, it captures money automatically.
+
+## License
+
+MIT
+
+---
+
+© 2025 RSC https://rsoftcon.com/
---
+++ src/providers/square-payment/service.ts
@@ -0,0 +1,120 @@
+import { AbstractPaymentProvider, MedusaError, PaymentActions } from "@medusajs/framework/utils"
+import { AuthorizePaymentInput, AuthorizePaymentOutput, CancelPaymentInput, CancelPaymentOutput, CapturePaymentInput, CapturePaymentOutput, DeletePaymentInput, DeletePaymentOutput, GetPaymentStatusInput, GetPaymentStatusOutput, InitiatePaymentInput, InitiatePaymentOutput, Logger, PaymentSessionStatus, ProviderWebhookPayload, RefundPaymentInput, RefundPaymentOutput, RetrievePaymentInput, RetrievePaymentOutput, UpdatePaymentInput, UpdatePaymentOutput, WebhookActionResult } from "@medusajs/framework/types"
+import { AbstractEventBusModuleService, defaultCurrencies } from "@medusajs/utils";
+import { Square, SquareClient, SquareEnvironment } from "square";
+import { randomUUID } from 'crypto';
+
+type Options = {
+  token: string,
+  environment?: string,
+}
+
+type InjectedDependencies = {
+  logger: Logger,
+  event_bus: AbstractEventBusModuleService
+}
+
+class SquarePaymentProviderService extends AbstractPaymentProvider<
+  Options
+> {
+
+  static identifier = "square-payment"
+
+  protected logger_: Logger
+  protected options_: Options
+  protected square_: SquareClient;
+  protected eventBusService_: AbstractEventBusModuleService
+  constructor(
+    container: InjectedDependencies,
+    options: Options
+  ) {
+    super(container, options)
+
+    this.logger_ = container.logger
+    this.options_ = options
+    this.eventBusService_ = container.event_bus
+
+    this.square_ = new SquareClient({
+      token: this.options_.token,
+      environment: this.options_.environment === 'sandbox' ? SquareEnvironment.Sandbox : SquareEnvironment.Production
+    })
+  }
+
+  static validateOptions(options: Record<any, any>) {
+    if (!options.token) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "token is required in the provider's options."
+      )
+    }
+    if (options.environment) {
+      if (options.environment !== 'production' && options.environment !== "sandobx") {
+        throw new MedusaError(
+          MedusaError.Types.INVALID_DATA,
+          "environment needs to be either production or sandbox"
+        )
+      }
+    }
+  }
+
+  async capturePayment(paymentData: CapturePaymentInput): Promise<CapturePaymentOutput> {
+    return {
+      data: paymentData,
+    }
+  }
+
+  async authorizePayment(paymentData: AuthorizePaymentInput): Promise<AuthorizePaymentOutput> {
+    return {
+      ...paymentData,
+      status: paymentData.data && paymentData.data.status === 'COMPLETED' ? 'captured' : 'pending',
+    }
+  }
+  async cancelPayment(paymentData: CancelPaymentInput): Promise<CancelPaymentOutput> {
+    throw new Error("cancelPayment Method not implemented.")
+  }
+  async initiatePayment(paymentData: InitiatePaymentInput): Promise<InitiatePaymentOutput> {
+    const result = await this.square_.payments.create({
+      idempotencyKey: randomUUID(),
+      sourceId: paymentData.data!.token as string,
+      amountMoney: {
+        currency: paymentData.currency_code.toUpperCase() as Square.Currency,
+        // currency: "USD",
+        amount: BigInt(paymentData.amount as number * 100)
+      }
+    })
+
+    if (result.payment && result.payment.id) {
+      return {
+        id: result.payment.id,
+        data: {
+          ...paymentData,
+          status: result.payment.status,
+        }
+      }
+    }
+
+    throw new Error("initpayment no context provided")
+  }
+  async deletePayment(paymentSessionData: DeletePaymentInput): Promise<DeletePaymentOutput> {
+    throw new Error("deletePayment Method not implemented.")
+  }
+  async getPaymentStatus(paymentData: GetPaymentStatusInput): Promise<GetPaymentStatusOutput> {
+    throw new Error("getPaymentStatus Method not implemented.")
+  }
+  async refundPayment(paymentData: RefundPaymentInput): Promise<RefundPaymentOutput> {
+    throw new Error("refundPayment Method not implemented.")
+  }
+  async retrievePayment(paymentSessionData: RetrievePaymentInput): Promise<RetrievePaymentOutput> {
+    throw new Error("retrievePayment Method not implemented.")
+  }
+  async updatePayment(context: UpdatePaymentInput): Promise<UpdatePaymentOutput> {
+    throw new Error("updatePayment Method not implemented.")
+  }
+  async getWebhookActionAndData(data: ProviderWebhookPayload["payload"]): Promise<WebhookActionResult> {
+    return {
+      action: PaymentActions.NOT_SUPPORTED
+    }
+  }
+}
+
+export default SquarePaymentProviderService
